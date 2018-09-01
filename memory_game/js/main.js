5c57a0ec10d4ad6ea08e8e8a68284e4a6fc243dd
wenhao <wh_wang@live.com>
2018-09-02 02:01:34 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,24 +4,20 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne;
-
-cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped "+ cardsInPlay); 
-
-var cardTwo;
-
-cardTwo = cards[1];
-
-cardsInPlay.push(cardTwo);
-
-
-if (cardsInPlay.length == 2 && cardsInPlay[0]==cardsInPlay[1]) {
-	alert("You found a match!");
+var checkForMatch = function() {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	}
+	else {
+		console.log("Sorry, try again.");
+	}
 }
-else {
-	alert("Sorry, try again");
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 }
+flipCard(0);
+flipCard(2);
